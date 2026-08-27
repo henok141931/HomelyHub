65a0848172c5778c5dca895ec31668c0e0f1bbe3
Henok Niguse <Henok.Niguse@MMCY.TECH>
2026-08-27 10:47:37 -0400

    authentication

--- backend/src/Models/userModel.js
+++ backend/src/Models/userModel.js
@@ -12,5 +12,31 @@ const userSchema = new mongoose.Schema({
     //
     trim: true,
     maxLength:[50, "your name can not be longer than 50 characters"]
-   } 
+   } ,
+email:{
+type:string,
+required:[true,"please enter email ID"],
+unique:true,
+lowercase:true,
+trim:true,
+validate:[validator.isEmail, "Please enter valid email"]
+
+
+},
+
+password:{
+type:string,
+required: [true,"Please enter  password"],
+minlength:[6,"your password must be longer than 6 characters"],
+select:false
+
+},
+
+passwordConfirm:{
+   type:string,
+   required:[true,"Please confirm your password"],
+}
+
+
+
 })
